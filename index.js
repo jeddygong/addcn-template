3f8565e67bf24fb8561df70d49cca89e6f90f413
Jeddy <jed.gong.nine@gmail.com>
2021-11-02 21:43:56 +0800

    update: version

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports ={
+  'template-A' : {
+    url: 'https://github.com/dongtaotao/template-A',
+    downloadUrl: 'http://github.com:dongtaotao/template-A#master',
+    description: 'A模版'
+  },
+  'template-B' : {
+    url: 'https://github.com/dongtaotao/template-B',
+    downloadUrl: 'http://github.com:dongtaotao/template-B#master',
+    description: 'B模版'
+  },
+  'template-C' : {
+    url: 'https://github.com/dongtaotao/template-C',
+    downloadUrl: 'http://github.com:dongtaotao/template-C#master',
+    description: 'C模版'
+  },
+};
